959a3e2609b2be82f60f4a9800c34ae563eb4900
qkrtkdgus010208 <psh010208@naver.com>
2025-06-15 21:44:34 +0900

    Ch04-01
    게임의 오브젝트 관리

--- Assets/Script/Manager/GamePoolManager.cs
+++ Assets/Script/Manager/GamePoolManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class GamePoolManager
 {
@@ -17,13 +18,46 @@ public class GamePoolManager
         }
     }
 
+    private Dictionary<SkillType, Queue<SkillBase>> SkillPool;
+
     public void Init()
     {
-
+        SkillPool = new Dictionary<SkillType, Queue<SkillBase>>();
     }
 
     public void Clear()
     {
+        SkillPool.Clear();
+        SkillPool = null;
+    }
 
+    public void EnqueueSkillPool(SkillBase skill)
+    {
+        if (SkillPool == null)
+        {
+            return;
+        }
+
+        if (SkillPool.ContainsKey(skill.SkillType) == false)
+        {
+            SkillPool.Add(skill.SkillType, new Queue<SkillBase>());
+        }
+
+        SkillPool[skill.SkillType].Enqueue(skill);
+    }
+
+    public SkillBase DequeueSkillPool(SkillType skillType)
+    {
+        if (SkillPool == null)
+        {
+            return null;
+        }
+
+        if (SkillPool.ContainsKey(skillType) == false)
+        {
+            return null;
+        }
+
+        return SkillPool[skillType].Dequeue();
     }
 }
---
+++ Assets/Script/Skill/SkillBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillBase : MonoBehaviour
+{
+    public SkillType SkillType { get; set; }
+
+    public virtual void FireSkill(Vector3 startPos, Vector3 startDir)
+    {
+
+    }
+
+    public virtual void StopSkill()
+    {
+        gameObject.SetActive(false);
+
+        GamePoolManager.Instance.EnqueueSkillPool(this);
+    }
+
+    public virtual void Update()
+    {
+        
+    }
+
+    public virtual void OnDestory()
+    {
+
+    }
+
+}
